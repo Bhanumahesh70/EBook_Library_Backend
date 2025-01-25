df333ebb7c0c73fb6527159eb577430fa051d4c0
Bhanumahesh Pasham <bhanumaheshpasham@BhanumaheshsMBP.cable.rcn.com>
2025-01-24 20:57:28 -0600

    added on book JPA test class

--- src/main/java/com/ebook/domain/Publisher.java
+++ src/main/java/com/ebook/domain/Publisher.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -45,6 +46,13 @@ public class Publisher extends AbstractClass {
         this.phoneNumber = phoneNumber;
     }
 
+    public void addBook(Book book){
+        if(this.getBooks()==null){
+            books = new ArrayList<Book>();
+        }
+        books.add(book);
+        this.setBooks(books);
+    }
     @Override
     public String toString() {
         return "Publisher{" +
--- src/test/java/com/ebook/domain/JPATest/BookJPATest.java
+++ src/test/java/com/ebook/domain/JPATest/BookJPATest.java
@@ -11,8 +11,8 @@ public class BookJPATest extends AbstractJPATest{
 
         Book book = new Book(1999,2,100,"english","JungleBook","harry","2000000000000");
         Publisher publisher = new Publisher("publisher","address","publisher@gmail.com","1234567890");
-        book.setPublisher(publisher);
         persistEntity(publisher);
+        book.setPublisher(publisher);
         persistEntity(book);
         Book readFromDb = findEntity(Book.class, book.getId());
         assertNotNull(readFromDb);
@@ -24,11 +24,11 @@ public class BookJPATest extends AbstractJPATest{
     public void readTest(){
         Book book = new Book(1990,20,200,"english","HarryPotter","jhon","3000000000000");
         Publisher publisher = new Publisher("publisher","address","publisher@gmail.com","1234567890");
-        book.setPublisher(publisher);
-        publisher.setBooks(book);
         persistEntity(publisher);
+        book.setPublisher(publisher);
+       // publisher.addBook(book);
         persistEntity(book);
-        Book findBook = em.createQuery("SELECT b FROM b WHERE b.title = 'HarryPotter'", Book.class).getSingleResult();
+        Book findBook = em.createQuery("SELECT b FROM Book b WHERE b.title = 'HarryPotter'", Book.class).getSingleResult();
         assertNotNull(findBook);
         assertEquals("HarryPotter",findBook.getTitle());
     }
@@ -37,14 +37,15 @@ public class BookJPATest extends AbstractJPATest{
     public void updateTest(){
         Book book = new Book(1970,300,1200,"english","LordOfTheRing","raju","5000000000000");
         Publisher publisher = new Publisher("publisher","address","publisher@gmail.com","1234567890");
+        persistEntity(publisher);
         book.setPublisher(publisher);
         persistEntity(book);
-        Book findBook = em.createQuery("SELECT b FROM b WHERE b.title = 'HarryPotter'", Book.class).getSingleResult();
+        Book findBook = em.createQuery("SELECT b FROM Book b WHERE b.title = 'LordOfTheRing'", Book.class).getSingleResult();
         tx.begin();
-        findBook.setTitle("Lord of The Rings ");
+        findBook.setTitle("Lord of The Rings");
         em.merge(findBook);
         tx.commit();
-        Book updatedBook = findEntity(Book.class, findBook.getId());
+        Book updatedBook = findEntity(Book.class, book.getId());
         assertEquals("Lord of The Rings",updatedBook.getTitle() );
     }
 
@@ -52,6 +53,7 @@ public class BookJPATest extends AbstractJPATest{
     public void deleteTest(){
         Book book = new Book(2004,50,5000,"english","PeterEngland","peter","7000000000000");
         Publisher publisher = new Publisher("publisher","address","publisher@gmail.com","1234567890");
+        persistEntity(publisher);
         book.setPublisher(publisher);
         persistEntity(book);
         Book findBook = findEntity(Book.class, book.getId());
